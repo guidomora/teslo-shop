17e3f8d422717c2356c8fed41ac2474ac54f32a6
guidomora <guidomorabito161@hotmail.com>
2023-07-08 19:38:13 -0300

    8/7

---
+++ src/pages/api/products/[slug].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connect, disconnect } from '../../../../database/db'
+import { IProduct } from '@/Interfaces/products'
+import Product from '@/models/Product'
+
+type Data =
+    | { message: string }
+    | IProduct
+
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+
+    switch (req.method) {
+        case "GET":
+            return getProductBySlug(req, res)
+
+        default:
+            return res.status(400).json({
+                message: "Bad request"
+            })
+    }
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+    
+    await connect()
+    const { slug } = req.query
+    const product = await Product.findOne({ slug }).lean()
+    await disconnect()
+
+    if (!product) {
+        return res.status(404).json({ message: 'Producto no encontrado' })
+    }
+
+    return res.json(product)
+}
---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connect, disconnect } from '../../../../database/db';
+import { IProduct } from '@/Interfaces/products';
+import Product from '@/models/Product';
+import { SHOP_CONSTANTS } from '../../../../database/constants';
+
+type Data = 
+| { message: string }
+| IProduct[]
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case "GET":
+            return getProducts(req, res)
+
+        default:
+            return res.status(400).json({
+                message: "Bad request"
+            })
+    }
+
+
+}
+
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    // la query por defecto va a sr all (trae todos los productos)
+    const {gender = "all" } = req.query
+    let condition = {}
+
+    // si la query es diferente de all y es uno de los vlidateGenders
+    if (gender !== "all" && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+        condition = {gender}
+    }
+
+    await connect()
+    const products = await Product.find(condition)
+                                // el menos indica que no traiga eso -_id 
+                                .select("title images inStock slug -_id")
+                                .lean()
+    await disconnect()
+    return res.status(200).json(products)
+}
