18c88d332e3f75e97b35330a930ee3d7c140b754
guido morabito <guidomorabito161@hotmail.com>
2023-07-10 23:35:35 -0300

    SWR

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -4,7 +4,7 @@ import useSWR, { SWRConfiguration } from 'swr';
 
 
 
-const fetcher = (...args:[key:string]) => fetch(...args).then((res) => res.json())
+// const fetcher = (...args:[key:string]) => fetch(...args).then((res) => res.json())
 
 
 
